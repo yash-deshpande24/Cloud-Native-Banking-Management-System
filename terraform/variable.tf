d060d0578a1c0a550ca9fc5d25525ea86d360ebd
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 23:51:43 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,35 @@
-{
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+output "vpc_id" {
+
+  value = module.vpc.vpc_id
+
+}
+
+output "ec2_public_ip" {
+
+  value = module.ec2.ec2_public_ip
+
+}
+
+output "rds_endpoint" {
+
+  value = module.rds.rds_endpoint
+
+}
+
+output "s3_bucket_id" {
+
+  value = module.s3.bucket_id
+
+}
+
+output "alb_dns_name" {
+
+  value = module.alb.alb_dns_name
+
+}
+
+output "autoscaling_group_name" {
+
+  value = module.autoscaling.autoscaling_group_name
+
 }
