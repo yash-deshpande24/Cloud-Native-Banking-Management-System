e61d3a71e519d3c24d710a2d1dd00631bb11a5f4
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 23:55:46 +0530

    Update main.tf

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -1,1 +1,92 @@
+resource "aws_security_group" "ec2_sg" {
+
+    name = "banking-ec2-sg-new"
+
+    description = "Security Group for EC2"
+
+    vpc_id = var.vpc_id
+
+ingress {
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+ingress {
+
+    from_port = 3000
+
+    to_port = 3000
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+ingress {
+
+    from_port = 5000
+
+    to_port = 5000
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+}
+
+resource "aws_security_group" "alb_sg" {
+
+    name = "banking-alb-sg-new"
+
+    description = "Security Group for ALB"
+
+    vpc_id = var.vpc_id
+
+ingress {
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+}
 
