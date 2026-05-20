0ab95c03efb9ac2f1316a7d8862e9e1dd46de420
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 14:24:20 +0530

    Update api.js

--- Frontend/public/src/services/api.js
+++ Frontend/public/src/services/api.js
@@ -1,65 +1,9 @@
-import React from "react";
+import axios from "axios";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+const API = axios.create({
 
-import Navbar from "./components/Navbar";
+  baseURL: "http://localhost:5000/api"
 
-import Home from "./pages/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Dashboard from "./components/Dashboard";
-import Transactions from "./components/Transactions";
-import Account from "./pages/Account";
+});
 
-function App() {
-
-  return (
-
-    <BrowserRouter>
-
-      <Navbar />
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/transactions"
-          element={<Transactions />}
-        />
-
-        <Route
-          path="/account"
-          element={<Account />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-
-  );
-}
-
-export default App;
+export default API;
