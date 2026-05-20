860e00f2b3d0618ed747a5c96f998996dd088872
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-21 00:05:59 +0530

    Update main.tf

--- terraform/modules/autoscaling/main.tf
+++ terraform/modules/autoscaling/main.tf
@@ -1,5 +1,30 @@
-{
-    aws {
-        region = "us-east-1"
-    }
+resource "aws_launch_template" "banking_template" {
+
+    name_prefix = "banking-template"
+
+    image_id = var.ami_id
+
+    instance_type = "t3.micro"
+
 }
+
+resource "aws_autoscaling_group" "banking_asg" {
+
+    desired_capacity = 2
+
+    max_size = 3
+
+    min_size = 1
+
+    vpc_zone_identifier = var.subnet_ids
+
+launch_template {
+
+    id = aws_launch_template.banking_template.id
+
+    version = "$Latest"
+
+    }
+
+}
+
