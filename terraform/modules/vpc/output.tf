50f7c77b67decc2ecb59b2dcbcc1f229469a70d7
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 23:53:13 +0530

    Create output.tf

---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,17 @@
+output "vpc_id" {
+
+    value = aws_vpc.banking_vpc.id
+
+}
+
+output "public_subnet_1_id" {
+
+    value = aws_subnet.public_subnet_1.id
+
+}
+
+output "public_subnet_2_id" {
+
+    value = aws_subnet.public_subnet_2.id
+
+}
