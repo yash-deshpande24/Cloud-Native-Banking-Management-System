6e750a17cd1d329abb79f5828deaeba0c6cb9318
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 23:52:45 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,68 @@
-{
-    aws {
-        region = "us-east-1"
+resource "aws_vpc" "banking_vpc" {
+    cidr_block = var.vpc_cidr
+
+    tags = {
+    Name = "banking-vpc"
     }
+
+}
+
+resource "aws_subnet" "public_subnet_1" {
+
+    vpc_id = aws_vpc.banking_vpc.id
+
+    cidr_block = "10.0.10.0/24"
+
+    availability_zone = "us-east-1a"
+
+    map_public_ip_on_launch = true
+
+}
+
+resource "aws_subnet" "public_subnet_2" {
+
+    vpc_id = aws_vpc.banking_vpc.id
+
+    cidr_block = "10.0.2.0/24"
+
+    availability_zone = "us-east-1b"
+
+    map_public_ip_on_launch = true
+
+}
+
+resource "aws_internet_gateway" "igw" {
+
+    vpc_id = aws_vpc.banking_vpc.id
+
+}
+
+resource "aws_route_table" "public_rt" {
+
+    vpc_id = aws_vpc.banking_vpc.id
+
+route {
+
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.igw.id
+
+    }
+
+}
+
+resource "aws_route_table_association" "public_assoc_1" {
+
+    subnet_id = aws_subnet.public_subnet_1.id
+
+    route_table_id = aws_route_table.public_rt.id
+
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+
+    subnet_id = aws_subnet.public_subnet_2.id
+
+    route_table_id = aws_route_table.public_rt.id
+
 }
