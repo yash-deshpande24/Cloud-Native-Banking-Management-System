e922a9fb27a159d223dd1df44e0cb76e450718b4
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 23:58:46 +0530

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,5 +1,19 @@
-{
-    aws {
-        region = "us-east-1"
+resource "aws_db_instance" "banking_rds" {
+    allocated_storage = 20
+    engine = "mysql"
+    engine_version = "8.0"
+    
+    instance_class = var.instance_class
+    username = var.db_username
+    password = var.db_password
+    skip_final_snapshot = true
+
+    publicly_accessible = true
+
+    tags = {
+
+    Name = "banking-rds"
+
     }
+
 }
