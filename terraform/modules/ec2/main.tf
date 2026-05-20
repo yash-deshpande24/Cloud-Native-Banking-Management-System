152b0717412e01a851ed691a69dc27a7890ddc1a
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-21 00:02:35 +0530

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,9 +1,19 @@
-{
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+resource "aws_instance" "banking_ec2" {
+
+  ami           = var.ami_id
+
+  instance_type = var.instance_type
+
+  subnet_id     = var.subnet_id
+
+  key_name      = var.key_name
+
+  vpc_security_group_ids = var.security_group_ids
+
+  tags = {
+
+    Name = "banking-ec2"
+
   }
+
 }
