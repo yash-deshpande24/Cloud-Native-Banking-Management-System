cf2f3c948fa375d70695a660e2a371bb01a3b64e
YashDeshpande24 <155445472+yash-deshpande24@users.noreply.github.com>
2026-05-20 23:52:07 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,15 @@
-{
-    aws {
-        region = "us-east-1"
-    }
+terraform {
+  backend "s3" {
+
+    bucket = "banking-terraform-backend"
+
+    key = "terraform.tfstate"
+
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-lock"
+
+    encrypt = true
+  }
+
 }
